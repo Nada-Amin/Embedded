4a185b2200bf1be48a6c43b5e44c4a182478a4b1
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2023-02-14 15:19:32 +0200

    Add files via upload

---
+++ STD_Types.h
@@ -0,0 +1,23 @@
+/*
+ * STD_Types.h
+ *
+ *  Created on: Feb 9, 2023
+ *  Author: Nada
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+//typedef used as alias for data types
+typedef  unsigned char                uint8;//8 bits
+typedef  signed char                  sint8;
+typedef  unsigned short int              uint16;//16 bits
+typedef  signed short int              sint16;
+typedef  unsigned long int             uint32;//32 bits
+typedef  signed long int               sint32;
+typedef  unsigned long long int          uint64;//64 bits
+typedef  signed long long             sint64;
+typedef  float                        float32;
+typedef  double                       float64;
+
+
+#endif /* STD_TYPES_H_ */
